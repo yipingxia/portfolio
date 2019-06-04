4cf8913e6ac5e6e22df1667b8eb346451c94437d
Eva, Xia Yiping <eva.ping2506@gmail.com>
2019-06-05 01:16:24 +0800

    Updated the about pages: 	Updated some font selections, content & styling; 	Hiding the image for now; 	Add accordion JS; WIP main page: 	Landing screen; 	Content & styling;

--- js/custom.js
+++ js/custom.js
@@ -60,4 +60,21 @@ $(window).load(function () {
             $(this).next("span").removeClass("active");
         }
     });
+
+    // accordian
+    var acc = document.getElementsByClassName("accordion");
+
+    var i;
+
+    for (i = 0; i < acc.length; i++) {
+      acc[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+          panel.style.display = "none";
+        } else {
+          panel.style.display = "block";
+        }
+      });
+    }
 });
